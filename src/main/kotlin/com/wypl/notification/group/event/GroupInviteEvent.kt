05c0265d0857ee7aa8fea03e7be33d7c25f11e30
KIMSEI1124 <workju1124@gmail.com>
2024-08-10 23:37:33 +0900

    Refactor(#8): 클래스 이름 수정

--- src/main/kotlin/com/wypl/notification/group/event/GroupInviteEvent.kt
+++ src/main/kotlin/com/wypl/notification/group/event/GroupInviteEvent.kt
@@ -1,12 +1,12 @@
-package com.wypl.notification.group.message
+package com.wypl.notification.group.event
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.wypl.notification.global.kafka.KafkaConsumerMessage
+import com.wypl.notification.global.kafka.KafkaConsumerEvent
 
-data class GroupInviteMessage(
+data class GroupInviteEvent(
     @JsonProperty("sender_id") val senderId: Long,
     @JsonProperty("recipient_id") val recipientId: Long,
     @JsonProperty("nickname") val nickname: String,
     @JsonProperty("group_id") val groupId: Long,
     @JsonProperty("group_name") val groupName: String,
-) : KafkaConsumerMessage()
+) : KafkaConsumerEvent()
---
+++ src/main/kotlin/com/wypl/notification/group/event/GroupKafkaConsumerEventType.kt
@@ -0,0 +1,5 @@
+package com.wypl.notification.group.event
+
+enum class GroupKafkaConsumerEventType {
+    GROUP_INVITE,
+}
